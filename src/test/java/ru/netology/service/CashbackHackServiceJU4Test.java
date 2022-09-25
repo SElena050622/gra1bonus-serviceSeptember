b87dc5e907e6a5e962ef9447690b64fa855a05ae
Сем Елена <semova.s81@bk.ru>
2022-09-25 19:01:38 +0300

    made junit4

---
+++ src/test/java/ru/netology/service/CashbackHackServiceJU4Test.java
@@ -0,0 +1,59 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+
+public class CashbackHackServiceJU4Test {
+
+    @Test
+    public void mustBeAddTo1900() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1900);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void mustBeAddTo900() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(900);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void mustBeAddTo1100() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1100);
+        int expected = 900;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void mustBeAddTo1000() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(0);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void noNeedToAddUpTo2000() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(2000);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void noNeedToAddUpTo1000() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+}
