d5291224e0e4f9f7a7a48b3b84c748fb80794cb6
Сем Елена <semova.s81@bk.ru>
2022-09-25 19:21:58 +0300

    2 tests are falling

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -31,18 +31,26 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void mustBeAddTo2000() {
+    public void mustBeAddTo1000() {
         CashbackHackService service = new CashbackHackService();
-        int actual = service.remain(2000);
+        int actual = service.remain(0);
         int expected = 1000;
         assertEquals(actual, expected);
     }
 
     @Test
-    public void mustBeAddTo1000() {
+    public void noNeedToAddUpTo2000() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(2000);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void noNeedToAddUpTo1000() {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(1000);
-        int expected = 1000;
+        int expected = 0;
         assertEquals(actual, expected);
     }
 }
